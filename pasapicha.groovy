f3796fcf94320d04cff7cfe480ecbf22e7f44acd
Jorge <jagedn@gmail.com>
2017-03-01 17:28:11 +0100

    Create pasapicha.groovy

---
+++ pasapicha.groovy
@@ -0,0 +1,5 @@
+print 'Remote Groovy script @ https://github.com/jagedn/groovyscripts/new/master/pasapicha.groovy'
+ 
+println "Ahora mismo en tu maquina son las ${new Date().format('yyyy-MM-dd HH:mm:ss')}"
+
+println "a que si ?"
