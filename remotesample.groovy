e7346bdecd4266973c738841aed5494915a48b07
Jorge <jagedn@gmail.com>
2017-03-01 17:19:59 +0100

    Create remotesample.groovy

---
+++ remotesample.groovy
@@ -0,0 +1,16 @@
+// File:
+// http://www.mrhaki.com/samples/remotesample.groovy
+ 
+printMessage 'Remote Groovy script @ http://www.mrhaki.com/samples/remotesample.groovy'
+ 
+['Groovy', 'rocks'].each {
+    print "${it.toUpperCase()} "
+}
+println '!'
+ 
+def printMessage(message) {
+    def LINE_LENGTH = message.size() + 4
+    println '*' * LINE_LENGTH
+    println "* $message *"
+    println '*' * LINE_LENGTH
+}
